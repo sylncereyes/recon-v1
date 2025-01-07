6f678b1eee0956d6f4372eb6be25fbf1dd7987cb
sylncereyes <70642783+sylncereyes@users.noreply.github.com>
2025-01-08 03:33:04 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -31,7 +31,6 @@ clear
 echo "### DNS Resolution - Resolve Discovered Subdomains" | tee -a "$LOGFILE"
 puredns resolve subdomains.txt -r ~/.wordlists/resolvers.txt -w resolved.txt 2>> "$LOGFILE"
 dnsx -l resolved.txt -silent -json -o dns.json | jq -r '.a?[]?' | anew ips.txt >> "$LOGFILE"
-dnsx -l subdomains.txt -silent -json -o dns2.json | jq -r '.a?[]?' | anew ips.txt >> "$LOGFILE"
 sleep 2
 clear
 
@@ -42,12 +41,6 @@ tew -x nmap.xml -dnsx dns.json --vhost -o ports.txt | httpx -silent -json -o htt
 cat http.json | jq -r '.url' | sed -e 's/:80$//g' -e 's/:443$//g' | sort -u | anew http.txt >> "$LOGFILE"
 tew -x nmap.xml -dnsx --vhost | anew ports.txt | httpx -silent -json -o http2.json 2>> "$LOGFILE"
 cat http2.json | jq -r '.url' | sed -e 's/:80$//g' -e 's/:443$//g' | sort -u | anew http.txt >> "$LOGFILE"
-
-nmap -T4 -vv -iL subdomains.txt --top-ports 3000 -n --open -oX nmap2.xml 2>> "$LOGFILE"
-tew -x nmap2.xml -dnsx dns2.json --vhost | anew ports.txt | httpx -silent -json -o http3.json 2>> "$LOGFILE"
-cat http3.json | jq -r '.url' | sed -e 's/:80$//g' -e 's/:443$//g' | sort -u | anew http.txt >> "$LOGFILE"
-tew -x nmap2.xml -dnsx --vhost | anew ports.txt | httpx -silent -json -o http4.json 2>> "$LOGFILE"
-cat http4.json | jq -r '.url' | sed -e 's/:80$//g' -e 's/:443$//g' | sort -u | anew http.txt >> "$LOGFILE"
 sleep 2
 clear
 
@@ -57,6 +50,8 @@ cat subdomains.txt | httpx -silent -sc | grep 200 | cut -d" " -f1 | anew hosts.t
 whatweb -i subdomains.txt | grep "200 OK" | cut -d" " -f1 | anew urls.txt >> "$LOGFILE"
 sed 's/\x1b\[[0-9;]*m//g' urls.txt | awk -F/ '{print $1 "//" $3}' | sort -u | anew hosts.txt >> "$LOGFILE"
 cat http.txt | anew hosts.txt >> "$LOGFILE"
+rm -rf urls.txt >> "$LOGFILE"
+cat hosts.txt | httpx -silent -sc -td -ip -o info.txt >> "$LOGFILE"
 sleep 2
 clear
 
